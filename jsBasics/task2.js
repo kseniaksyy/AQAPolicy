872c466ddf5714e674c949de4eafe0c98673541f
kseniaksyyy <kseniaksyyy@gmail.com>
2025-08-10 13:18:13 +0300

    Homework lesson 4

---
+++ jsBasics/task2.js
@@ -0,0 +1,10 @@
+let name1 = "Batuan"
+let name2 = "Joker"
+
+// Конкатенація рядків
+let viva = "Hola " + name1 + " & " + name2 + "!" 
+console.log (viva)
+
+// Шаблонний рядок
+let viva1 = `Hola ${name1} & ${name2}!`
+console.log (viva1)
---
+++ jsBasics/task4_3.js
@@ -0,0 +1,5 @@
+let radius = 4
+let height = 18
+let volume = Math.Pi * radius ** 2 * height 
+
+console.log ("Volume of cylinder is:", volume.toFixed(2))
