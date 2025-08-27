20e8fd11dccb4a7e7c27804b444bb0847cec3a1f
kseniaksyy <kseniaksyyy@gmail.com>
2025-08-27 21:29:39 +0300

    HW 8

---
+++ jsArrays/task5.js
@@ -0,0 +1,4 @@
+const firstArray = ["Paris", "London", "Berlin"]
+const secondArray = ["Mariupol", "Kharkiv", "Odesa"]
+const concatenatedArray = firstArray.concat(secondArray);
+console.log(concatenatedArray)
