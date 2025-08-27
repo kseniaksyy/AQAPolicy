002933c0d0c5e304a2acb5223c1ee2ca8283d55c
kseniaksyy <kseniaksyyy@gmail.com>
2025-08-27 22:45:59 +0300

    upd HW 8

--- jsArrays/task6.js
+++ jsArrays/task6.js
@@ -1,4 +1,4 @@
 const numbersList = [1,10,14,2,4,5,43,34]
 const sortedList = [...numbersList].sort((a, b) => a-b)
 console.log("Масив:", numbersList);
-console.log("Відсортований масив:", sortedList);
+console.log("Відсортований масив: ", sortedList);
