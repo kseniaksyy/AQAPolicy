96197ec745cea87f173ef5492ba120346e4cf51b
kseniaksyy <kseniaksyyy@gmail.com>
2025-08-30 19:27:27 +0300

    home work 9

---
+++ jsObjects/task3.js
@@ -0,0 +1,15 @@
+const car1 = {
+    brand: "Toyota",
+    model: "Camry",
+    year: "2021"
+}
+
+const car2 = {
+    brand: "Ford",
+    model: "Fusion",
+    owner: "Me"
+}
+
+const car3 = {...car1, ...car2}
+
+console.log(car3);
