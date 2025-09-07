dd167afb50e9af9288b3f52bba41949661591155
kseniaksyy <kseniaksyyy@gmail.com>
2025-09-07 18:46:43 +0300

    Home work 10

---
+++ JS_OOP1/Book.js
@@ -0,0 +1,56 @@
+export default class Book {
+    constructor(author, title, year) {
+        this.author = author
+        this.title = title
+        this.year = year
+    }
+    get author() {
+        return this._author
+    }
+    get title() {
+        return this._title
+    }
+    get year() {
+        return this._year
+    }
+
+    set author(value) {
+        if (typeof value !== 'string' || value.trim() === '') {
+            throw new Error("Невірний формат імені автора")
+        }
+        this._author = value
+    }
+
+    set title(value) {
+        if (typeof value !== 'string' || value.trim() === '') {
+            throw new Error("Невірний фомат назви книги")
+        }
+        this._title = value
+    }
+
+    set year(value) {
+        if (typeof value !== 'number' || value <= 0) {
+            throw new Error("Невірний формат року");
+        }
+        this._year = value;
+    }
+
+
+    printInfo() {
+        return `Автор: ${this.author} Назва книги: ${this.title} Рік видання: ${this.year}`;
+    }
+
+    static findOldestBook(books) {
+        if (!Array.isArray(books) || books.length === 0) return null;
+        return books.reduce((oldest, current) =>
+            current.year < oldest.year ? current : oldest
+        );
+    }
+}
+
+
+
+// const result = `${book1.printInfo()}
+// ${book2.printInfo()}
+// ${book3.printInfo()}`;
+// console.log(result);
