7cbd6f7163b31b04800daf9e03897a242378756a
kseniaksyy <kseniaksyyy@gmail.com>
2025-10-01 04:32:41 +0300

    Add Account and Books controllers with authentication and book management methods

---
+++ JS_AdvAPI/accountController.js
@@ -0,0 +1,18 @@
+const BaseController = require('./baseController.js');
+class AccountController extends BaseController {
+	async getAuthToken(userName, password) {
+		const response = await this.axiosInstance.post('/Account/v1/GenerateToken', {
+			userName: userName,
+			password: password,
+		});
+		return response.data.token;
+	}
+	async getUserId(userName, password) {
+		const response = await this.axiosInstance.post('/Account/v1/Login', {
+			userName: userName,
+			password: password,
+		});
+		return response.data.userId;
+	}
+}
+module.exports = new AccountController();
